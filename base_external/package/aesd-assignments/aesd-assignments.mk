1731d0e593b3c67a2ced65f5e585815ea4657a6d
ram066 <rrkumar066@gmail.com>
2026-03-19 01:03:42 +0530

    Assignment 4 part 2 buildroot  implementation

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -11,7 +11,9 @@ AESD_ASSIGNMENTS_VERSION =  main
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = git@github.com:ram066/assignments-3-and-later-ram066.git
+
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-ram066.git
+
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
